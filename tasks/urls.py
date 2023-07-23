66ac38e4652594ac92b3fb1719c6cea5d16250ac
Svitlana Savikhina <savihina111@gmail.com>
2023-07-23 17:31:38 +0300

    Create TaskDeleteView & task_confirm_delete.html

--- tasks/urls.py
+++ tasks/urls.py
@@ -4,13 +4,14 @@ from django.urls import path
 from tasks.views import (
     TodoListView,
     TaskCreateView,
-    TaskUpdateView
+    TaskUpdateView, TaskDeleteView
 )
 
 urlpatterns = [
     path("", TodoListView.as_view(), name="index"),
     path("tasks/create/", TaskCreateView.as_view(), name="task-create"),
     path("tasks/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
+    path("tasks/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
 
 ]
 
